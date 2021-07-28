a94bacceda8affbd29bb3b339afd27e761ea542c
Ram Parameswaran <ramanan.parameswaran@gmail.com>
2021-07-29 02:03:10 +1000

    Add General success tests for RecordLabelListView

--- src/record_label/tests/test_views.py
+++ src/record_label/tests/test_views.py
@@ -1,3 +1,4 @@
+import pytest
 import requests
 
 from django.test import RequestFactory, TestCase
@@ -39,3 +40,124 @@ class TestRecordLabelListView_ApiRequestHandling(TestCase):
         resp = RecordLabelListView(request)
 
         assert resp.status_code == 429
+
+
+@patch(
+    "requests.get",
+)
+class TestRecordLabelListView(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    @pytest.mark.django_db
+    def test_view_success(self, mock_api_request):
+        # Tests a success case using sample data.
+
+        api_data = [
+            {
+                "name": "Alpha Festival",
+                "bands": [
+                    {"name": "Band A", "recordLabel": "Record Label 2"},
+                ],
+            },
+            {
+                "name": "Beta Festival",
+                "bands": [
+                    {"name": "Band A", "recordLabel": "Record Label 2"},
+                ],
+            },
+            {
+                "name": "Omega Festival",
+                "bands": [
+                    {"name": "Band X", "recordLabel": "Record Label 1"},
+                ],
+            },
+        ]
+        mock_api_request.return_value = mock_response(200, api_data)
+
+        request = self.factory.get("/")
+        resp = RecordLabelListView(request)
+
+        assert resp.status_code == 200
+        assert resp.data == [
+            {
+                "label": "Record Label 1",
+                "bands": [
+                    {"name": "Band X", "festivals": [{"name": "Omega Festival"}]}
+                ],
+            },
+            {
+                "label": "Record Label 2",
+                "bands": [
+                    {
+                        "name": "Band A",
+                        "festivals": [
+                            {"name": "Alpha Festival"},
+                            {"name": "Beta Festival"},
+                        ],
+                    }
+                ],
+            },
+        ]
+
+    @pytest.mark.django_db
+    def test_view_success_with_missing_data(self, mock_api_request):
+        # Tests that the View function handles missing data, and preserves order
+        # correctly.
+
+        api_data = [
+            {
+                "name": "Alpha Festival",
+                "bands": [
+                    {"name": "Band A"},
+                ],
+            },
+            {
+                "name": "Beta Festival",
+                "bands": [
+                    {"name": "Band A", "recordLabel": "Record Label 2"},
+                ],
+            },
+            {
+                "name": "Omega Festival",
+                "bands": [
+                    {"name": "Band X", "recordLabel": "Record Label 1"},
+                ],
+            },
+        ]
+        mock_api_request.return_value = mock_response(200, api_data)
+
+        request = self.factory.get("/")
+        resp = RecordLabelListView(request)
+
+        assert resp.status_code == 200
+        assert resp.data == [
+            {
+                "label": None,
+                "bands": [
+                    {
+                        "name": "Band A",
+                        "festivals": [
+                            {"name": "Alpha Festival"},
+                        ],
+                    }
+                ],
+            },
+            {
+                "label": "Record Label 1",
+                "bands": [
+                    {"name": "Band X", "festivals": [{"name": "Omega Festival"}]}
+                ],
+            },
+            {
+                "label": "Record Label 2",
+                "bands": [
+                    {
+                        "name": "Band A",
+                        "festivals": [
+                            {"name": "Beta Festival"},
+                        ],
+                    }
+                ],
+            },
+        ]
--- src/record_label/utils.py
+++ src/record_label/utils.py
@@ -1,5 +1,6 @@
-from record_label.models import Band, BandLabel, MusicFestival, RecordLabel
+from django.db import transaction
 
+from record_label.models import Band, BandLabel, MusicFestival, RecordLabel
 from record_label.serializers import RecordLabelSerializer
 
 
@@ -11,7 +12,7 @@ def restructure_data(festivals_api_data):
     - `festivals_api_data`: a list of Python objects
 
     :returns:
-    - `record_labels`: a list of Python objects with the following schema:
+    - `out`: Serialized output (JSON) with the following schema:
         [
             {"label": <str>,
              "bands": [
@@ -21,33 +22,46 @@ def restructure_data(festivals_api_data):
                       ]
             },
         ]
+
+    :note:
+    - this function creates (and then destroys) DB object.
+    - this is to take advantage of Django ORM for relationship mapping, and
+      Django ModelSerializers for JSON serialization.
+    - Writing to the DB may not be necessary.
+    - However I note that in production, we would likely persist the API
+      data on our DB to reduce unncessary repeated compute.
+
     """
 
-    for festival in festivals_api_data:
-        # Create MusicFestival object in ORM
-        festival_obj, _ = MusicFestival.objects.get_or_create(name=festival.get("name"))
-
-        bands = festival.get("bands", [])
-
-        for band in bands:
-            band_obj, _ = Band.objects.get_or_create(name=band.get("name"))
-            label_obj, _ = RecordLabel.objects.get_or_create(
-                name=band.get("recordLabel")
+    # Ensure atomic transactions (for speed and security so concurrent requests have independent state)
+    with transaction.atomic():
+        for festival in festivals_api_data:
+            # Create MusicFestival object in ORM
+            festival_obj, _ = MusicFestival.objects.get_or_create(
+                name=festival.get("name")
             )
 
-            bandlabel_obj, _ = BandLabel.objects.get_or_create(
-                band=band_obj, recordLabel=label_obj
-            )
-            festival_obj.bands.add(bandlabel_obj)
+            bands = festival.get("bands", [])
 
-    out = RecordLabelSerializer(
-        RecordLabel.objects.all().order_by("name"), many=True
-    ).data
+            for band in bands:
+                band_obj, _ = Band.objects.get_or_create(name=band.get("name"))
+                label_obj, _ = RecordLabel.objects.get_or_create(
+                    name=band.get("recordLabel")
+                )
 
-    # Cleanup DB
-    MusicFestival.objects.all().delete()
-    BandLabel.objects.all().delete()
-    Band.objects.all().delete()
-    RecordLabel.objects.all().delete()
+                bandlabel_obj, _ = BandLabel.objects.get_or_create(
+                    band=band_obj, recordLabel=label_obj
+                )
+                festival_obj.bands.add(bandlabel_obj)
+
+        out = RecordLabelSerializer(
+            RecordLabel.objects.all().order_by("name"), many=True
+        ).data
+
+        # Cleanup DB
+        MusicFestival.objects.all().delete()
+        BandLabel.objects.all().delete()
+        Band.objects.all().delete()
+        RecordLabel.objects.all().delete()
 
     return out
